2b592647d9b089efc1e61b855f5dc3994f8ce299
GitRaag7 <chirag.pathak1990@gmail.com>
2021-08-04 19:01:10 +0530

    Lec 48,49,50 working with state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,24 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  //const datex = new Date(2021, 6, 26);
-  //const loan = "Platinum loan";
-  //const value = 50000000;
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated");
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">Rs.{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Click Me</button>
     </Card>
   );
 }
